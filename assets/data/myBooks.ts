6766bf5a33ee3fb2d66cf377d88b8fa0b6961a07
burgesda <52463507+burgesda@users.noreply.github.com>
2019-07-23 15:01:19 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,6 @@
+import {Book} from '../../app/book';
+
+export const MYBOOKS: Book[] = [{name: "The Fellowship Of the Ring", author: "J.R.R. Tolkien", genre: "Fantasy", yearPub: 1954, picture: "assets/images/fellowship.jpg"},
+{name: "Brave New World", author: "Aldous Huxley", genre: "Science Fiction", yearPub: 1932, picture: "assets/images/brave.jpg"},
+{name: "The Return Of The King", author: "J.R.R. Tolkien", genre: "Fantasy", yearPub: 1955, picture: "assets/images/king.jpg"},
+{name: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", genre: "Science Fiction", yearPub: 1979, picture: "assets/images/galaxy.jpg"}]
